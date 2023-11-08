8135886dfc1fe239b7ccd04f7fadac433372f2e6
Goran Jovic <goran1010jovic@gmail.com>
2023-11-08 18:05:24 +0100

    Add insert method

--- src/classTree.js
+++ src/classTree.js
@@ -1,6 +1,7 @@
 import buildTree from "./buildTree.js";
 import removeDuplicates from "./removeDuplicates.js";
 import mergeSort from "./mergeSort.js";
+import Node from "./classNode.js";
 
 export default class Tree {
   constructor(array) {
@@ -23,6 +24,27 @@ export default class Tree {
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+  insert(insertData, currentNode = this.root) {
+    if (!this.root) {
+      this.root = new Node(insertData);
+      return;
+    }
+    if (insertData === currentNode.data) return;
+    if (insertData < currentNode.data) {
+      if (!currentNode.left) {
+        currentNode.left = new Node(insertData);
+        return;
+      }
+      return this.insert(insertData, currentNode.left);
+    }
+    if (insertData > currentNode.data) {
+      if (!currentNode.right) {
+        currentNode.right = new Node(insertData);
+        return;
+      }
+      return this.insert(insertData, currentNode.right);
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const testTree = new Tree(testArray);
 
 testTree.prettyPrint();
+
+testTree.insert(2);
+testTree.insert(10000);
+testTree.insert(-2);
+testTree.insert(63);
+testTree.insert(-12);
+testTree.insert(-52);
+
+testTree.prettyPrint();
